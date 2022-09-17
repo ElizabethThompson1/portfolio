78c83c6656249d689d7263ca081de088572bca08
ElizabethThompson1 <usawsoccer11@yahoo.com>
2022-09-16 20:18:31 -0500

    added @media to nav and home page

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 let btn = document.getElementById("btn");
+let smallNav = document.getElementById("smallNav-btn")
 
 btn.addEventListener("click", function () {
     window.location.href = "thankYou.html"
+})
+
+smallNav.addEventListener("click", function () {
+    this.style.display = block
 })
